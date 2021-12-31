128c02fed9a35612c56bae17feaeb6ff7a321b4e
gavinspr <gavin.sproles@gmail.com>
2021-12-31 13:08:04 -0500

    heroku deploy

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,22 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-// @Injectable()
-// export class AppService {
-//   getHello(): string {
-//     return 'Hello World!';
-//   }
-// }
-
-
-import { Resolver, Query, Args } from '@nestjs/graphql';
-// import { LegendNFTService } from './legendnft.service';
-// import { LegendType } from './legendnft.dto';
-// import { InjectEthersProvider } from 'nestjs-ethers';
-
+import { Resolver, Query } from '@nestjs/graphql';
 @Resolver()
 export class AppService {
-  // constructor(private readonly legendnftService: LegendNFTService) {}
-
   @Query((returns) => String)
   async getHello() {
     return 'Hello World!';
