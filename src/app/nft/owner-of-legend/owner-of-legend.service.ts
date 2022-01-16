bec72455c8714ed25bd3db9900f25392f622e3e3
gavinspr <gavin.sproles@gmail.com>
2022-01-16 14:57:32 -0500

    fixups owner-of-legend

--- src/app/nft/owner-of-legend/owner-of-legend.service.ts
+++ src/app/nft/owner-of-legend/owner-of-legend.service.ts
@@ -5,9 +5,21 @@ import { contractLab as lab } from 'src/contract-lab/contract-lab.service';
 @Injectable()
 export class OwnerOfLegendService {
   async fetchOwnerOfLegend(id: string): Promise<OwnerOfLegend> {
-    const ownerOfLegend: any = {};
+    let address: string;
 
-    ownerOfLegend['address'] = await lab.nft.ownerOf(id);
+    try {
+      address = await lab.nft.ownerOf(id);
+    } catch (e) {
+      if (e.reason === 'ERC721: owner query for nonexistent token') {
+        address = 'Nonexistent Legend ID';
+      } else {
+        throw new Error('Unknown API Error Occurred');
+      }
+    }
+
+    const ownerOfLegend: OwnerOfLegend = {
+      address: address,
+    };
 
     return ownerOfLegend;
   }
