496a7acc9897fe8d466a61c9c8584c2d9cef45c7
gavinspr <gavin.sproles@gmail.com>
2022-01-16 13:31:34 -0500

    clean up and fixups is-listable

--- src/app/nft/is-listable/is-listable.model.ts
+++ src/app/nft/is-listable/is-listable.model.ts
@@ -3,7 +3,7 @@ import { ObjectType, Field } from '@nestjs/graphql';
 @ObjectType()
 export class IsListable {
   @Field()
-  isListable: boolean;
+  listable: boolean;
 
   @Field({ nullable: true })
   unableReason?: string;
--- src/app/nft/is-listable/is-listable.service.ts
+++ src/app/nft/is-listable/is-listable.service.ts
@@ -5,21 +5,30 @@ import { contractLab as lab } from 'src/contract-lab/contract-lab.service';
 @Injectable()
 export class IsListableService {
   async fetchIsListable(id: string, address: string): Promise<IsListable> {
-    const isListable: any = {};
-
     if ((await lab.admin.isHatched(id)) === false) {
-      isListable['isListable'] = false;
-      isListable['unableReason'] = 'Legend NFT Not Hatched';
+      const isListable: IsListable = {
+        listable: false,
+        unableReason: 'Legend NFT Not Hatched',
+      };
 
       return isListable;
     }
+
+    let listable: boolean;
+    let unableReason: string;
+
     if ((await lab.nft.ownerOf(id)) === address) {
-      isListable['isListable'] = true;
+      listable = true;
     } else {
-      isListable['isListable'] = false;
-      isListable['unableReason'] =
+      listable = false;
+      unableReason =
         'Unauthorized Caller: Caller is not owner of the Legend NFT.';
     }
+
+    const isListable: IsListable = {
+      listable: listable,
+      unableReason: unableReason,
+    };
 
     return isListable;
   }
